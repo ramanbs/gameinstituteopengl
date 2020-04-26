35c610a3b7b378219a52489738ec1dd52fd72bb8
ramanbs <bsraman1991@gmail.com>
2020-04-27 00:58:18 +0530

    Lighting 1 : Added reading of normals from .obj and added vbo info for it. Also modified the loading of .obj files to make it more robust and extensible. Updated notes regarding lighting , Phong Model and Lambert's law of reflectance.

--- OpenGLGameInstitute/src/Mesh.cpp
+++ OpenGLGameInstitute/src/Mesh.cpp
@@ -4,6 +4,24 @@
 #include <fstream>
 
 
+std::vector<std::string> split(std::string s, std::string t) 
+{
+	std::vector<std::string> res;
+	while (1)
+	{
+		int pos = s.find(t);
+		
+		if (pos == -1) 
+		{
+			res.push_back(s);
+			break;
+		}
+		res.push_back(s.substr(0, pos));
+		s = s.substr(pos + 1, s.size() - pos - 1);
+	}
+
+	return res;
+}
 
 Mesh::Mesh()
 	:mLoaded(false)
@@ -19,9 +37,10 @@ Mesh::~Mesh()
 
 bool Mesh::loadOBJ(const std::string& filename) 
 {
-	std::vector<unsigned int> vertexIndices, uvIndices;
+	std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
 	std::vector<glm::vec3> tempVertices;
 	std::vector<glm::vec2> tempUVs;
+	std::vector<glm::vec3> tempNormals;
 
 	if (filename.find(".obj") != std::string::npos) 
 	{
@@ -36,52 +55,83 @@ bool Mesh::loadOBJ(const std::string& filename)
 		std::cout << "Loading OBJ file " << filename << "..." << std::endl;
 
 		std::string lineBuffer;
+		
 		while (std::getline(fin, lineBuffer)) 
 		{
-			if (lineBuffer.substr(0, 2) == "v ")
+			std::stringstream ss(lineBuffer);
+			std::string cmd;
+
+			ss >> cmd;
+
+			if (cmd == "v")
 			{
-				std::istringstream v(lineBuffer.substr(2));
+				
 				glm::vec3 vertex;
 				//std::get() does not ignore whitespace whereas extraction operator does
-				v >> vertex.x;
-				v >> vertex.y;
-				v >> vertex.z;
+				int dim = 0;
+				while (dim < 3 && ss >> vertex[dim])
+					dim++;
 
 				tempVertices.push_back(vertex);
 			}
-			else if (lineBuffer.substr(0, 2) == "vt")
+			else if (cmd == "vt")
 			{
-				std::istringstream vt(lineBuffer.substr(3));
 				glm::vec2 uv;
 				
-				vt >> uv.s; 
-				vt >> uv.t;
+				int dim = 0;
+				while (dim < 2 && ss >> uv[dim])
+					dim++;
 				
 				tempUVs.push_back(uv);
 			}
-			else if (lineBuffer.substr(0, 2) == "f ")
+			else if (cmd == "vn")
 			{
-				int p1, p2, p3;
-				int t1, t2, t3;
-				int n1, n2, n3;
+				glm::vec3 normal;
 
-				const char* face = lineBuffer.c_str();
-				//sscanf_s is a Microsoft compiler version of sscanf that reads formatted input from a string. This variant is helpful as it warns the users if the value assigned overflows when assigned to a variable, returns number of data it found and read
-				int match = sscanf_s(face, "f %i/%i/%i %i/%i/%i %i/%i/%i",
-					&p1, &t1, &n1,
-					&p2, &t2, &n2,
-					&p3, &t3, &n3);
+				int dim = 0;
+				while (dim < 3 && ss >> normal[dim])
+					dim++;
 
-				if (match != 9)
-					std::cout << "Failed to parse OBJ file using our very simple OBJ loader" << std::endl;
-				//We are igonring normals for now 
-				vertexIndices.push_back(p1);
-				vertexIndices.push_back(p2);
-				vertexIndices.push_back(p3);
+				normal = glm::normalize(normal);
 
-				uvIndices.push_back(t1);
-				uvIndices.push_back(t2);
-				uvIndices.push_back(t3);
+				tempNormals.push_back(normal);
+			}
+			else if (cmd == "f")
+			{
+				std::string faceData;
+				int vertexIndex, uvIndex, normalIndex; // v/vt/vn 
+
+				while (ss >> faceData)
+				{
+					std::vector<std::string> data = split(faceData, "/");
+					
+					// vertex index
+					if (data[0].size() > 0) 
+					{
+						sscanf_s(data[0].c_str(), "%d", &vertexIndex);
+						vertexIndices.push_back(vertexIndex);
+					}
+
+					// if the face vertex has a texture coordinate index 
+					if (data.size() >= 1) 
+					{
+						if (data[1].size() > 0) 
+						{
+							sscanf_s(data[1].c_str(), "%d", &uvIndex);
+							uvIndices.push_back(uvIndex);
+						}
+					}
+
+					// if the face vertex has a normal index 
+					if (data.size() >= 2) 
+					{
+						if (data[2].size() > 0)
+						{
+							sscanf_s(data[2].c_str(), "%d", &normalIndex);
+							normalIndices.push_back(normalIndex);
+						}
+					}
+				}
 
 			}
 		}
@@ -91,13 +141,25 @@ bool Mesh::loadOBJ(const std::string& filename)
 		// For each vertex of the triangle
 		for (unsigned int i = 0; i < vertexIndices.size(); i++) 
 		{
-			// Get the attributes using the indices
-			glm::vec3 vertex = tempVertices[vertexIndices[i] - 1];
-			glm::vec2 uv = tempUVs[uvIndices[i] - 1];
-
 			Vertex meshVertex;
-			meshVertex.position = vertex;
-			meshVertex.texCoords = uv;
+
+			if (tempVertices.size() > 0)
+			{
+				glm::vec3 vertex = tempVertices[vertexIndices[i] - 1];
+				meshVertex.position = vertex;
+			}
+			
+			if (tempNormals.size() > 0)
+			{
+				glm::vec3 normal = tempNormals[normalIndices[i] - 1];
+				meshVertex.normal = normal;
+			}
+			
+			if (tempUVs.size() > 0)
+			{
+				glm::vec2 uv = tempUVs[uvIndices[i] - 1];
+				meshVertex.texCoords = uv;
+			}
 
 			mVertices.push_back(meshVertex);
 		}
@@ -140,13 +202,18 @@ void Mesh::initBuffers()
 
 	// Create vertex attribute (always after we have bound the vao as these attrib works on the current vao) - for shaders to interpret the vertex data (how the vertex data is laid out in the buffer)
 	// 0 (index) - attribute identifier, 3 - number of components that constitute this attribute (x, y, z), GL_FLOAT - type of data, GL_FALSE - need to normalize the data in screen space,  0 - stride (continuous bytes of data that constitute the given attribute incase the vertext data is interleaved with some other data in the buffer like e.g. color), NULL - offset from which the actual data starts for this attrib in the buffer. 
+	
 	//position attrib pointer
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, NULL); // holds info passed to this function in the VAO, so we have to generate VAO first.
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL); // holds info passed to this function in the VAO, so we have to generate VAO first.
 	glEnableVertexAttribArray(0); // 0 - attrib index, by default opengl disables vertex attrib array.
 
-	//texture attrib
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 5, (GLvoid*)((sizeof(GLfloat) * 3)));
+	// Normals Attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)((sizeof(GLfloat) * 3)));
 	glEnableVertexAttribArray(1);
+	
+	//texture attrib
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)((sizeof(GLfloat) * 6)));
+	glEnableVertexAttribArray(2);
 
 	// Unbind vertex array
 	glBindVertexArray(0);
